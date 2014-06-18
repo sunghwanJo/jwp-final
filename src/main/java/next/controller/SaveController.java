a7f7a9039f5b2e74ab5091458863bfe084567508
sunghwan <namul10@gmail.com>
2014-06-18 17:21:23 +0900

    3. Redirect Code 활

--- src/main/java/next/controller/SaveController.java
+++ src/main/java/next/controller/SaveController.java
@@ -12,28 +12,20 @@ import core.mvc.Controller;
 public class SaveController implements Controller {
 	private QuestionDao questionDao = new QuestionDao();
 	private List<Question> questions;
-	
+
 	@Override
-	public String execute(HttpServletRequest request, HttpServletResponse response)
-			throws Exception {
+	public String execute(HttpServletRequest request,
+			HttpServletResponse response) throws Exception {
 		String writer, title, contents;
-		
-		
+
 		writer = request.getParameter("writer");
 		title = request.getParameter("title");
 		contents = request.getParameter("contents");
-		
+
 		Question question = new Question(writer, title, contents);
 		questionDao.insert(question);
-		
-		/*
-		 * ListController 로 Redirect를 시키는 방법으로 리팩토링해야할듯..?
-		 */
-		questions = questionDao.findAll();
-		request.setAttribute("questions", questions);
-		return "list.jsp";
+
+		return "redirect:/list.next";
 	}
-
-
 
 }
